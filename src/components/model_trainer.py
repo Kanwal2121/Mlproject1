41aca5558b715adef50965247d12f75483c87191
Kanwal2121 <sinhgkanwal2121@gmail.com>
2025-06-13 18:05:28 +0530

    Hyper Parameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -32,8 +32,64 @@ class ModelTrainer:
                         'RandomForestRegressor':RandomForestRegressor(),
                         'KNeighborsRegressor':KNeighborsRegressor(),
                         'GradientBoostingRegressor':GradientBoostingRegressor()
+                    }
+            regressor_hyperparams ={
+                        "LinearRegression": {
+                            "fit_intercept": [True, False],
+                            "positive": [True, False]
+                        },
+                        "Ridge": {
+                            "alpha": [0.01, 0.1, 1.0, 10.0, 100.0],
+                            "solver": ["auto", "svd", "cholesky", "lsqr", "sag"]
+                        },
+                        "Lasso": {
+                            "alpha": [0.0001, 0.001, 0.01, 0.1, 1.0],
+                            "max_iter": [1000, 5000, 10000]
+                        },
+                        "SVR": {
+                            "kernel": ["linear", "poly", "rbf", "sigmoid"],
+                            "C": [0.1, 1.0, 10.0],
+                            "gamma": ["scale", "auto"]
+                        },
+                        "DecisionTreeRegressor": {
+                            "max_depth": [None, 5, 10, 20],
+                            "min_samples_split": [2, 5, 10],
+                            "min_samples_leaf": [1, 2, 4]
+                        },
+                        "RandomForestRegressor": {
+                            "n_estimators": [100, 200, 500],
+                            "max_depth": [None, 10, 20],
+                            "min_samples_split": [2, 5, 10],
+                            "min_samples_leaf": [1, 2, 4]
+                        },
+                        "KNeighborsRegressor": {
+                            "n_neighbors": [3, 5, 7],
+                            "weights": ["uniform", "distance"],
+                            "p": [1, 2]  
+                        },
+                        "GradientBoostingRegressor": {
+                            "n_estimators": [100, 200],
+                            "learning_rate": [0.01, 0.05, 0.1],
+                            "max_depth": [3, 5, 7],
+                            "subsample": [0.8, 1.0]
+                        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             }
-            model_report:dict=model_evaluation(X_train=X_train_Data,X_test=X_test_Data,y_train=y_train_Data,y_test=y_test_Data,models=models)
+            model_report:dict=model_evaluation(X_train=X_train_Data,X_test=X_test_Data,y_train=y_train_Data,y_test=y_test_Data,models=models,params=regressor_hyperparams)
             best_model_score=max(sorted(model_report.values()))
             
             for key,value in model_report.items():
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -20,12 +20,20 @@ def save_object(file_path,obj):
         
 
 
-def model_evaluation(X_train,X_test,y_train,y_test,models):
+def model_evaluation(X_train,X_test,y_train,y_test,models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=params[list(params.keys())[i]]
+            gs=GridSearchCV(model,para,cv=3)
+
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+
+
             y_predict=model.predict(X_test)
 
             Score=r2_score(y_test,y_predict)
