536a02eee464368da4ea93fc025c230671080705
Kanwal2121 <sinhgkanwal2121@gmail.com>
2025-06-14 17:28:38 +0530

    Last Commit

--- application.py
+++ application.py
@@ -41,7 +41,8 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-#if __name__=="__main__":
-    #application.run(host="0.0.0.0")        
+if __name__=="__main__":
+    application.run(host="0.0.0.0")  
+          
 
 
